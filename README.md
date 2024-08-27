f5000c8e521e801138338f6bfa3ed3097083af23
Chinonye Ekpendu <alexekpendu001@gmail.com>
2024-08-26 21:14:37 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# 40HTML-Test-questions
---
+++ eventListener.js
@@ -0,0 +1,50 @@
+const text1 = document.querySelector(".answer1-txt");
+const change1 = document.querySelector(".answer1-btn");
+change1.addEventListener("click", function(){
+    text1.classList.add("change1");
+})
+const text2 = document.querySelector(".answer2-txt");
+const change2 = document.querySelector(".answer2-btn");
+change2.addEventListener("click", function(){
+    text2.classList.add("change2");
+})
+const text3 = document.querySelector(".answer3-txt");
+const change3 = document.querySelector(".answer3-btn");
+change3.addEventListener("click", function(){
+    text3.classList.add("change3");
+})
+const text4 = document.querySelector(".answer4-txt");
+const change4 = document.querySelector(".answer4-btn");
+change4.addEventListener("click", function(){
+    text4.classList.add("change4");
+})
+const text5 = document.querySelector(".answer5-txt");
+const change5 = document.querySelector(".answer5-btn");
+change5.addEventListener("click", function(){
+    text5.classList.add("change5");
+})
+const text6 = document.querySelector(".answer6-txt");
+const change6 = document.querySelector(".answer6-btn");
+change6.addEventListener("click", function(){
+    text6.classList.add("change6");
+})
+const text7 = document.querySelector(".answer7-txt");
+const change7 = document.querySelector(".answer7-btn");
+change7.addEventListener("click", function(){
+    text7.classList.add("change7");
+})
+const text8 = document.querySelector(".answer8-txt");
+const change8 = document.querySelector(".answer8-btn");
+change8.addEventListener("click", function(){
+    text8.classList.add("change8");
+})
+const text9 = document.querySelector(".answer9-txt");
+const change9 = document.querySelector(".answer9-btn");
+change9.addEventListener("click", function(){
+    text9.classList.add("change9");
+})
+const text10 = document.querySelector(".answer10-txt");
+const change10 = document.querySelector(".answer10-btn");
+change10.addEventListener("click", function(){
+    text10.classList.add("change10");
+})
